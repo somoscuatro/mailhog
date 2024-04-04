3e2b316b39ca0ba8c7bd6c3ee7e9deda1283e326
Joan López <joan@somoscuatro.es>
2024-04-04 12:51:39 +0200

    Fixed code style.

--- src/class-plugin.php
+++ src/class-plugin.php
@@ -18,15 +18,19 @@ class Plugin {
 	 * @implements init
 	 */
 	public static function init() {
-		add_filter( 'phpmailer_init', function($php_mailer)
-			{
-				$php_mailer->Host = 'mailhog';
-				$php_mailer->Port = 1025;
-				$php_mailer->From = 'wordpress@localhost.test';
+		add_filter(
+			'phpmailer_init',
+			function( $php_mailer ) {
+				// phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+				$php_mailer->Host     = 'mailhog';
+				$php_mailer->Port     = 1025;
+				$php_mailer->From     = 'wordpress@localhost.test';
 				$php_mailer->FromName = 'WordPress';
 				$php_mailer->IsSMTP();
+				// phpcs:enable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			},
-		10);
+			10
+		);
 		add_filter( 'wp_mail_from', fn() => 'wordpress@localhost.test' );
 		add_filter( 'wp_mail_from_name', fn() => 'localhost' );
 	}
